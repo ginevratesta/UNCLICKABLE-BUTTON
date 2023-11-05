3ea80c56ce7fb4facc07eb731b03e4f5a4fcaf36
Ginevra Testa <ginevra99testaa@gmail.com>
2023-11-05 20:37:01 +0100

    last commit

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
+const yes = document.querySelector("#yesButton");
 const setPlay = document.querySelector("#button0");
+const newButton = document.createElement("button");
+const title = document.querySelector("h1");
 const container = document.querySelector("#container");
-const newButton = document.createElement('button');
 let counter = 0;
 
 function setRandomPosition(ev) {
-    const maxX = container.clientWidth - (newButton.clientWidth + setPlay.clientWidth);
-    const maxY = container.clientHeight - (newButton.clientHeight + setPlay.clientHeight);
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-    
-    console.log(ev.target);
+  const maxX =
+    container.clientWidth - (newButton.clientWidth + setPlay.clientWidth);
+  const maxY =
+    container.clientHeight - (newButton.clientHeight + setPlay.clientHeight);
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
 
-    if (ev.target === setPlay) {
-        setPlay.style.top = `${randomY}px`;
-        setPlay.style.left = `${randomX}px`;
-    } else if (ev.target === newButton) {
-        const newButtonX = container.clientWidth - (newButton.clientWidth + setPlay.clientWidth);
-        const newButtonY = container.clientHeight - (newButton.clientHeight + setPlay.clientHeight);
-        const newButtonRandomX = Math.random() * newButtonX;
-        const newButtonRandomY = Math.random() * newButtonY;
-        
-        newButton.style.top = `${newButtonRandomY}px`;
-        newButton.style.left = `${newButtonRandomX}px`;
-    }
-    
-    counter++;
-    
-    if (counter === 3) {
-        container.appendChild(newButton);
-        newButton.innerText = setPlay.innerText;
-        newButton.id = "button1"
-        newButton.classList.add("crazyButton" , "modify")
-        newButton.addEventListener("mouseover", setRandomPosition);
-    }
+  console.log(ev.target);
+
+  if (ev.target === setPlay) {
+    setPlay.style.top = `${randomY}px`;
+    setPlay.style.left = `${randomX}px`;
+  } else if (ev.target === newButton) {
+    const newButtonX =
+      container.clientWidth - (newButton.clientWidth + setPlay.clientWidth);
+    const newButtonY =
+      container.clientHeight - (newButton.clientHeight + setPlay.clientHeight);
+    const newButtonRandomX = Math.random() * newButtonX;
+    const newButtonRandomY = Math.random() * newButtonY;
+
+    newButton.style.top = `${newButtonRandomY}px`;
+    newButton.style.left = `${newButtonRandomX}px`;
+  }
+
+  counter++;
+
+  if (counter === 3) {
+    container.appendChild(newButton);
+    newButton.innerText = "U SURE?";
+    newButton.id = "button1";
+    newButton.classList.add("crazyButton", "modify");
+    newButton.addEventListener("mouseover", setRandomPosition);
+  }
+  if (ev.target === yes) {
+    title.innerText = "I Love You Too!";
+  }
 }
 
 setPlay.addEventListener("mouseover", setRandomPosition);
+yes.addEventListener("click", setRandomPosition);
