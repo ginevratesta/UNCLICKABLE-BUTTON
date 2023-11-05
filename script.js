2cf0cf5c68d52795a138bcfe06ce64d7924d2a65
Ginevra Testa <ginevra99testaa@gmail.com>
2023-11-05 19:08:39 +0100

    work in progress

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 const setPlay = document.querySelector(".crazyButton");
 const container = document.querySelector("#container");
 const newButton = document.createElement('button');
-let counter = 0
+let counter = 0;
 
-function setRandomPosition() {
-        const maxX = container.clientWidth - setPlay.clientWidth;
-        const maxY = container.clientHeight - setPlay.clientHeight;
-        const randomX = Math.floor(Math.random() * maxX);
-        const randomY = Math.floor(Math.random() * maxY);
-        
-        setPlay.style.margin = `${randomY}px ${randomX}px`;
-        
-        counter++
-
-       if(counter === 3){
-        container.appendChild(newButton);
-        newButton.innerText = setPlay.innerText;
-        newButton.classList.add('crazyButton');
-        newButton.addEventListener("mouseover", setRandomPosition);
-    }
-    const newButtonX = container.clientWidth - newButton.clientWidth;
-    const newButtonY = container.clientHeight - newButton.clientHeight;
-    const newButtonRandomX = Math.floor(Math.random() * newButtonX);
-    const newButtonRandomY = Math.floor(Math.random() * newButtonY);
+function setRandomPosition(event) {
+  const maxX = container.clientWidth - (newButton.clientWidth + setPlay.clientWidth);
+  const maxY = container.clientHeight - (newButton.clientHeight + setPlay.clientHeight);
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+  
+  if (event.target === setPlay) {
+    setPlay.style.top = `${randomY}px`;
+    setPlay.style.left = `${randomX}px`;
+  } else if (event.target === newButton) {
+    const newButtonX = container.clientWidth - (newButton.clientWidth + setPlay.clientWidth);
+    const newButtonY = container.clientHeight - (newButton.clientHeight + setPlay.clientHeight);
+    const newButtonRandomX = Math.random() * newButtonX;
+    const newButtonRandomY = Math.random() * newButtonY;
     
-    newButton.style.margin = `${newButtonRandomY}px ${newButtonRandomX}px`;
+    newButton.style.top = `${newButtonRandomY}px`;
+    newButton.style.left = `${newButtonRandomX}px`;
+  }
+  
+  counter++;
 
+  if (counter === 3) {
+    container.appendChild(newButton);
+    newButton.innerText = setPlay.innerText;
+    newButton.classList.add('crazyButton');
+    newButton.addEventListener("mouseover", setRandomPosition);
+  }
 }
 
 setPlay.addEventListener("mouseover", setRandomPosition);
